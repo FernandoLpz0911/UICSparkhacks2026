a233df722f577fd118adaab4559d127c406f1604
flope8 <flope8@uic.edu>
2026-02-06 23:56:59 -0600

    created routing to the home page

--- app/lib/screens/home.dart
+++ app/lib/screens/home.dart
@@ -1,1 +1,30 @@
-/// Home page work here
+import 'package:app/widgets/navigate.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  final String title;
+
+  /// create a homepage instance that refreshes
+  const Home({Key? key, required this.title}) : super(key: key);
+
+  /// create the private home type and return a private home ui page
+  @override
+  _Home createState() => _Home();
+}
+
+/// class for the private home instance that refreshes 
+class _Home extends State<Home> {
+
+  /// actual ui of the home page
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title:const Text("HOME PAGE")),
+      body: Center(
+        child: NavigateButton(label: "Lobby", destination: Home(title: "Home",)
+        ),
+      )
+    );
+  }
+
+}
