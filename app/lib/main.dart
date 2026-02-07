782af7075f053488a6248d71c71f1b10980d8961
Joseph Abhilash <josephabilas234@gmail.com>
2026-02-07 11:02:56 -0600

    Home_page 2nd commit

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -2,7 +2,6 @@
 import 'package:flutter/material.dart';
 import 'package:app/widgets/navigate.dart';
 import 'package:app/screens/home.dart';
-
 void main() {
   runApp(const MyApp());
 }
@@ -16,42 +15,42 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,  
       //Calls HomePage and prints the contents of HomePage
-      home: HomePage()
+      home: HomePage(),
     );
   }
 }
 
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
 
-//   // This widget is the home page of your application. It is stateful, meaning
-//   // that it has a State object (defined below) that contains fields that affect
-//   // how it looks.
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
 
-//   // This class is the configuration for the state. It holds the values (in this
-//   // case the title) provided by the parent (in this case the App widget) and
-//   // used by the build method of the State. Fields in a Widget subclass are
-//   // always marked "final".
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
 
-//   final String title;
+  final String title;
 
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-//   void _incrementCounter() {
-//     setState(() {
-//       // This call to setState tells the Flutter framework that something has
-//       // changed in this State, which causes it to rerun the build method below
-//       // so that the display can reflect the updated values. If we changed
-//       // _counter without calling setState(), then the build method would not be
-//       // called again, and so nothing would appear to happen.
-//       _counter++;
-//     });
-//   }
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -95,7 +94,7 @@ class MyApp extends StatelessWidget {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
-            NavigateButton(label: "Home", destination: Home(title: "Home"))
+            NavigateButton(label: "Home", destination: MyHomePage(title: "Home"))
           ],
         ),
       ),
