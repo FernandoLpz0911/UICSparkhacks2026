3dd03411880ff67fd86c942d1985a5d08d239c88
Joseph Abhilash <josephabilas234@gmail.com>
2026-02-07 00:04:57 -0600

    First commit

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  // Remove this after setting up authenticaton
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -34,6 +41,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
--- app/lib/services/authentication.dart
+++ app/lib/services/authentication.dart
@@ -1,1 +1,32 @@
 /// Authentication work here
+
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService{
+    final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+    User? get currentUser => firebaseAuth.currentUser;   
+
+    Stream<User?> get authStateChanges => firebaseAuth.authStateChanges();
+
+    Future<void> signInWithEmailAndPassword(String email, String password) async {
+        try {
+            await firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+        } on FirebaseAuthException catch (e) {
+            throw e.message ?? 'An error occurred during sign in.';
+        }
+    }
+
+    Future<void> createAccountWithEmailAndPassword(String email, String password) async {
+        try {
+            await firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
+        } on FirebaseAuthException catch (e) {
+            throw e.message ?? 'An error occurred during account creation.';
+        }
+    }
+
+    Future<void>    signOut() async {
+        await firebaseAuth.signOut();
+    }
+    
+}
