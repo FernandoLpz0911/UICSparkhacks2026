0c84448353826d549e4bfdf06f52dfce86c0758a
flope8 <flope8@uic.edu>
2026-02-07 11:27:08 -0600

    Created a the gemini model service

---
+++ app/lib/services/gemini.dart
@@ -0,0 +1,20 @@
+import 'package:google_generative_ai/google_generative_ai.dart';
+import '../env/env.dart';
+
+class Gemini {
+  final _model = GenerativeModel(model: "gemini-2.5-flash-lite", apiKey: Env.apiKey);
+
+  /// 
+  /// Sends a message to the ai model with prompt as a string, and gets the
+  /// response back and returns it from the ai model
+  Future<String> sendMessage(String prompt) async {
+    try {
+      final content = [Content.text(prompt)];
+      final response = await _model.generateContent(content);
+
+      return response.text ?? "Gemini failed to return a text.";
+    } catch (e) {
+      return "Error: $e";
+    }
+  }
+}
